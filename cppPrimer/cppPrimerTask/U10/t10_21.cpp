0bbcb67aefec0b7f154124a2133f3364d0edc543
acormorant <acormorant@foxmail.com>
2025-09-28 19:41:59 +0800

    整理相关笔记

--- cppPrimer/cppPrimerTask/U10/t10_21.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int i=5;
-    auto f =[i]()mutable -> bool {if(i>0){--i;return false;}else return true;};
-    for(int j=0;j<6;j++){
-        cout <<f()<<" ";
-    }
-}
--- cppPrimer/cppPrimerTask/U6/Chapter6.h
+++
@@ -1,4 +0,0 @@
-#ifndef CHAPTER6_H
-#define CHAPTER6_H
-int fact(int );
-#endif //CHAPTER6_H
